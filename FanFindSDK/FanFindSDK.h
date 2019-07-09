ecdfedd6edb6cac802bffb93d364d9ae9140414f
Chris Woolum <chris@turnoutt.com>
2019-07-09 15:05:10 -0700

    Initial Commit

---
+++ FanFindSDK/FanFindSDK.h
@@ -0,0 +1,19 @@
+//
+//  FanFindSDK.h
+//  FanFindSDK
+//
+//  Created by Christopher Woolum on 7/9/19.
+//  Copyright © 2019 Turnoutt Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FanFindSDK.
+FOUNDATION_EXPORT double FanFindSDKVersionNumber;
+
+//! Project version string for FanFindSDK.
+FOUNDATION_EXPORT const unsigned char FanFindSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FanFindSDK/PublicHeader.h>
+
+
