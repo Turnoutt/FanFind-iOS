514a559b87c164cb40ede1639e70f70c17400b24
Chris Woolum <chris@turnoutt.com>
2021-06-30 17:43:20 -0700

    fix the build

--- build-universal-framework.sh
+++ build-universal-framework.sh
@@ -5,11 +5,12 @@ mkdir build
 
 # build framework for simulators
 xcodebuild clean build \
-  -project FanFindSDK.xcodeproj \
-  -scheme FanFindSDK \
-  -configuration Release \
-  -sdk iphonesimulator \
-  -derivedDataPath derived_data
+-project FanFindSDK.xcodeproj \
+-scheme FanFindSDK \
+-configuration Release \
+-sdk iphonesimulator \
+-derivedDataPath derived_data \
+EXCLUDED_ARCHS="arm64"
 
 # create folder to store compiled framework for simulator
 mkdir build/simulator
@@ -18,12 +19,12 @@ mkdir build/simulator
 cp -r derived_data/Build/Products/Release-iphonesimulator/FanFindSDK.framework build/simulator
 #build framework for devices
 xcodebuild clean build \
-  -project FanFindSDK.xcodeproj \
-  -scheme FanFindSDK \
-  -configuration Release \
-  -sdk iphoneos \
-  -derivedDataPath derived_data
-  
+-project FanFindSDK.xcodeproj \
+-scheme FanFindSDK \
+-configuration Release \
+-sdk iphoneos \
+-derivedDataPath derived_data
+
 # create folder to store compiled framework for simulator
 mkdir build/devices
 # copy compiled framework for simulator into our build folder
@@ -35,9 +36,9 @@ mkdir build/universal
 cp -r build/devices/FanFindSDK.framework build/universal/
 # create framework binary compatible with simulators and devices, and replace binary in unviersal framework
 lipo -create \
-  build/simulator/FanFindSDK.framework/FanFindSDK \
-  build/devices/FanFindSDK.framework/FanFindSDK \
-  -output build/universal/FanFindSDK.framework/FanFindSDK
-  
+build/simulator/FanFindSDK.framework/FanFindSDK \
+build/devices/FanFindSDK.framework/FanFindSDK \
+-output build/universal/FanFindSDK.framework/FanFindSDK
+
 # copy simulator Swift public interface to universal framework
 cp build/simulator/FanFindSDK.framework/Modules/FanFindSDK.swiftmodule/* build/universal/FanFindSDK.framework/Modules/FanFindSDK.swiftmodule
